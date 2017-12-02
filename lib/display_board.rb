d67c2ed8a757bf000f1cd12e9758991189a5725e
Robert Watson <spartan83@gmail.com>
2017-12-02 05:02:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board (toe=Array.new)
-board =" #{toe[0]} | #{toe[1]} | #{toe[2]} \n-----------\n #{toe[3]} | #{toe[4]} | #{toe[5]}
-\n-----------\n #{toe[6]} | #{toe[7]} | #{toe[8]} "
+def display_board
+board ="   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
 puts board
 end
